98aa82e949703287d904e72e51f570f0a7ac271f
Serhii Naboka <drifterzzz@gmail.com>
2023-03-16 17:51:23 +0200

    1st done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+const getBodyRandomBg = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+let intervalId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    getBodyRandomBg();
+  }, 1000);
+}
+function onStopBtnClick() {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  clearInterval(intervalId);
+}
