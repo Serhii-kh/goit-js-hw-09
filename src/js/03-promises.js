13a9c9ad56eeb459a0eb1e1f5b331ad30523e193
Serhii Naboka <drifterzzz@gmail.com>
2023-03-19 17:01:23 +0200

    3d done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let firstDelay = Number(e.currentTarget.delay.value);
+  const stepDelay = Number(e.currentTarget.step.value);
+  const numberOfPromises = Number(e.currentTarget.amount.value);
+
+  for (let i = 1; i <= numberOfPromises; i += 1) {
+    createPromise(i, firstDelay);
+
+    firstDelay += stepDelay;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  const promis = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promis
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
